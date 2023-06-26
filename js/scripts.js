10e22ed24ecadfad8e92d408cf840673b075f0de
stiva98 <samustiva9810@gmail.com>
2023-06-26 16:32:33 +0200

    FIX milestone 1

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,42 @@ const groupOfTheWork = [
         firstname: 'Wayne',
         lastname: 'Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
         firstname: 'Angela',
         lastname: 'Caroll',
         role: 'Chief Editor',
         image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
         firstname: 'Walter',
         lastname: 'Gordon',
         role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
         firstname: 'Angela',
         lastname: 'Lopez',
         role: 'Social Media Manager',
         image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
         firstname: 'Scott',
         lastname: 'Estrada',
         role: 'Developer',
         image: 'scott-estrada-developer.jpg',
+    },
+    {
         firstname: 'Barbara',
         lastname: 'Ramos',
         role: 'Graphic Designer',
         image: 'barbara-ramos-graphic-designer.jpg',
     }
-]
-console.log(groupOfTheWork)
+];
+//console.log(groupOfTheWork)
 //Stampo in console tutte le informazioni
-for (let i = 0; i > groupOfTheWork.length; i++){
+for (let i = 0; i < groupOfTheWork.length; i++){
     const element = groupOfTheWork[i];
     for (const key in element) {
         console.log(element[key]);
