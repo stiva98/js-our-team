4a00b6632205fa2a30866f09e94deb79d456c5a2
stiva98 <samustiva9810@gmail.com>
2023-06-26 16:07:44 +0200

    ADD milestone 0

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const groupOfTheWork = [
+    {
+        firstname: 'Wayne',
+        lastname: 'Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+        firstname: 'Angela',
+        lastname: 'Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+        firstname: 'Walter',
+        lastname: 'Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+        firstname: 'Angela',
+        lastname: 'Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+        firstname: 'Scott',
+        lastname: 'Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+        firstname: 'Barbara',
+        lastname: 'Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    }
+]
